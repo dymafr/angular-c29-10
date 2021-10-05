b7b577f16b33012fd82cce86f19fcdcd502fb236
dymafr <erwan1309@hotmail.com>
2021-10-05 14:31:21 +0200

    1 file modified

--- src/app/features/photos/shared/services/unsplash.service.ts
+++ src/app/features/photos/shared/services/unsplash.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { Photo } from '../interfaces/photo.interface';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UnsplashService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  public searchPhotos(search: string): Observable<Photo[]> {
+    return this.http.get<Photo[]>('');
+  }
 }
